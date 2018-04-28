41999fb300866a9993ccbd38114c579637a2a435
crzidea <i@crzidea.com>
2018-04-28 11:10:33 +0000

    Fix IP

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
-ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+# 208.67.222.222 = resolver1.opendns.com
+ip=$(dig +short myip.opendns.com @208.67.222.222)
 subnet="$ip/32"
 echo client subnet: $subnet
 dnsmasq \
